9719cd287fb27287923cb5cb4f3e5dd211737034
venegu <venegu@gmail.com>
2016-03-12 14:23:34 -0500

    Adding methods to post an image to parse

--- insty/Post.swift
+++ insty/Post.swift
@@ -7,7 +7,62 @@
 //
 
 import UIKit
+import Parse
 
 class Post: NSObject {
+    
+    /**
+     Method to post user media to Parse by uploading image file
+     
+     - parameter image: Image that the user wants upload to parse
+     - parameter caption: Caption text input by the user
+     - parameter completion: Block to be executed after save operation is complete
+     */
+    class func postUserImage(image: UIImage?, withCaption caption: String?, withCompletion completion: PFBooleanResultBlock?) {
+        // Create Parse object PFObject
+        let media = PFObject(className: "Post")
+        
+        print("Posting user image")
+        // Add relevant fields to the object
+        media["media"] = getPFFileFromImage(image) // PFFile column type
+        media["author"] = PFUser.currentUser() // Pointer column type that points to PFUser
+        media["caption"] = caption
+        media["likesCount"] = 0
+        media["commentsCount"] = 0
+        media["createdAt"] = "\(NSDate())"
+
+        // Save object (following function will save the object in Parse asynchronously)
+        media.saveInBackgroundWithBlock(completion)
+        print("user image save complete")
+    }
+    
+    /**
+     Method to post user media to Parse by uploading image file
+     
+     - parameter image: Image that the user wants to upload to parse
+     - returns: PFFile for the the data in the image
+     */
+    class func getPFFileFromImage(image: UIImage?) -> PFFile? {
+        // check if image is not nil
+        if let image = image {
+            // get image data and check if that is not nil
+            if let imageData = UIImagePNGRepresentation(image) {
+                return PFFile(name: "image.png", data: imageData)
+            }
+        }
+        return nil
+    }
+    
+    class func resize(image: UIImage, newSize: CGSize) -> UIImage {
+        let resizeImageView = UIImageView(frame: CGRectMake(0, 0, newSize.width, newSize.height))
+        resizeImageView.contentMode = UIViewContentMode.ScaleAspectFill
+        resizeImageView.image = image
+        
+        UIGraphicsBeginImageContext(resizeImageView.frame.size)
+        resizeImageView.layer.renderInContext(UIGraphicsGetCurrentContext()!)
+        let newImage = UIGraphicsGetImageFromCurrentImageContext()
+        UIGraphicsEndImageContext()
+        return newImage
+    }
 
 }
